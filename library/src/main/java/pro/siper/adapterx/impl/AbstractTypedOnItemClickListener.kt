08939f6a247b3571fbee52720476d4d7249b0c57
Siper <siper13@gmail.com>
2017-10-31 23:00:31 +0300

    Deleted unused interfaces

--- library/src/main/java/pro/siper/adapterx/impl/AbstractTypedOnItemClickListener.kt
+++
@@ -1,7 +0,0 @@
-package pro.siper.adapterx.impl
-
-
-interface AbstractTypedOnItemClickListener {
-    fun onClick(item: BaseItem, position: Int)
-    fun onLongClick(item: BaseItem, position: Int)
-}
