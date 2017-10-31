35a314f5607c6a53fbbc111415e39b180be6b9ef
Stersh <siper13@gmail.com>
2017-10-31 11:50:13 +0300

    Deleted useless abstract ViewHolderBinder

--- library/src/main/java/pro/siper/adapterx/ViewHolderBinder.kt
+++
@@ -1,8 +0,0 @@
-package pro.siper.adapterx
-
-import android.support.v7.widget.RecyclerView
-
-
-interface ViewHolderBinder<out T : RecyclerView.ViewHolder> {
-    fun bindView(holder: RecyclerView.ViewHolder): T
-}
