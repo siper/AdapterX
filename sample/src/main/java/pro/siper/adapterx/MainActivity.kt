d9774093725589a5497663cf5a1e86ef3e634ed8
Siper <siper13@gmail.com>
2017-11-01 06:52:32 +0300

    Sample update to version 2.x.x

--- sample/src/main/java/pro/siper/adapterx/MainActivity.kt
+++ sample/src/main/java/pro/siper/adapterx/MainActivity.kt
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.widget.Toast
+import pro.siper.adapterx.impl.BaseItem
+import pro.siper.adapterx.impl.BaseItemClickListener
 import pro.siper.adapterx.item.Item1
 import pro.siper.adapterx.item.Item2
 import pro.siper.adapterx.x.AdapterX
@@ -20,6 +22,18 @@ class MainActivity : AppCompatActivity() {
         adapter.addTypedOnItemLongClickListener(1, {
             item: Item1, position: Int -> Toast.makeText(applicationContext,
                 "Item 1 (${item.title}) at position: $position long clicked", Toast.LENGTH_SHORT).show()
+        })
+        adapter.setOnItemClickListener(object : BaseItemClickListener {
+            override fun onItemClick(item: BaseItem, position: Int) {
+                when (item) {
+                    is Item1 -> Toast.makeText(applicationContext,
+                            "Item 1 (${item.title}) at position: $position clicked", Toast.LENGTH_SHORT).show()
+                    is Item2 -> Toast.makeText(applicationContext,
+                            "Item 2 (${item.title}) at position: $position clicked", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onItemLongClick(item: BaseItem, position: Int) {}
         })
         recyclerView.adapter = adapter
         adapter.addItem(Item1("Monkey"))
