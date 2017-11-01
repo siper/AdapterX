412996b861774da1e7bbf1a47534f25faa690207
Siper <siper13@gmail.com>
2017-11-01 21:44:51 +0300

    Per page query parameter

--- sample/src/main/java/pro/siper/adapterx/model/api/UnsplashApi.kt
+++ sample/src/main/java/pro/siper/adapterx/model/api/UnsplashApi.kt
@@ -2,11 +2,11 @@ package pro.siper.adapterx.model.api
 
 import retrofit2.Call
 import retrofit2.http.GET
-import retrofit2.http.Path
 import retrofit2.http.Query
 
 
 interface UnsplashApi {
     @GET("photos/?")
-    fun listPhotos(@Query("client_id") client_id: String): Call<List<UnsplashItem>>
+    fun listPhotos(@Query("client_id") clientId: String,
+                   @Query("per_page") perPage: Int = 100): Call<List<UnsplashItem>>
 }
